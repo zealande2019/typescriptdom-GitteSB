9b90b1ec702e8aef9e540775f96ef5149bd29ed2
Gitte Sevel Bilenberg <gitt7083@edu.easj.dk>
2019-03-10 16:03:59 +0100

    commit

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,14 @@
-//Her skal du skrive din typescriptkode
+let Indkøbsliste = document.getElementById("listeUsund");
+let lielement = document.createElement("li");
+lielement.setAttribute("Id","4");
+lielement.setAttribute("Class","usundt");
+Indkøbsliste.appendChild(lielement);
+let CocaCola = document.createTextNode("Coca Cola");
+lielement.appendChild(CocaCola);
+lielement2.setAttribute("Id","5");
+lielement2.setAttribute("Class","usundt");
+let MatadorMix = document.createTextNode("Matador Mix");
+lielement2.appendChild(MatadorMix);
+
+
+
